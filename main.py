3ebb473470bfc19dfa52e781ccd2fba71fd3ab8c
HunterRedou <philipp.selke@gmail.com>
2025-03-23 23:33:46 +0100

    Added the Player Class and the Circleshape Class

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pygame
 from constants import *
+from player import Player
+
+
+
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_lock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.Surface.fill(screen, (0, 0, 0))#Black background
+        player.draw(screen)
         pygame.display.flip()
         
+        #limits the FPS
+        dt = fps_lock.tick(60)/1000
+        
+    
     print("Starting Asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
